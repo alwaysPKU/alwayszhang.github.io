26e952f1a4208f2927bf6bc380125731888ae292
alwaysqi <zw_pku@163.com>
2019-01-03 21:56:52 +0800

    firt

---
+++ _site/push.sh
@@ -0,0 +1,11 @@
+# ===========================================
+# --coding:UTF-8 --
+# file: do.sh
+# author: ZhengPengkun
+# date: 2018-12-11
+# email: pkzhengmath@pku.edu.cn
+# description: 
+# ===========================================
+git add .
+git commit -m $(date +%Y-%m-%d-%H-%M-%S)
+git push github master
