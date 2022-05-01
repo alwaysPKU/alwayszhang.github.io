fb129af2acb37a62b68e7d944913d02665e6e098
alwaysqi <zw_pku@163.com>
2022-05-01 13:56:29 +0800

    2022-05-01-13-56-29

---
+++ pull.sh
@@ -0,0 +1,9 @@
+# ===========================================
+# --coding:UTF-8 --
+# file: push.sh
+# author: william
+# date: 2019-1-4
+# email: v.cpp@pku.edu.cn
+# description: 
+# ===========================================
+git pull github master
