932a368c155c84dfc098ff5c8cc9f14ffa2fd982
用户3031733109593 <961445003792483-3031733109593@noreply.coze.cn>
2026-07-15 17:54:59 +0800

    fix: 使用 trailingSlash: false 生成 .html 文件

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,7 @@ const nextConfig: NextConfig = {
   // outputFileTracingRoot: path.resolve(__dirname, '../../'),  // Uncomment and add 'import path from "path"' if needed
   /* config options here */
   output: process.env.GITHUB_PAGES === 'true' ? 'export' : undefined,
-  trailingSlash: true,
+  trailingSlash: false,
   allowedDevOrigins: ['*.dev.coze.site'],
   images: {
     remotePatterns: [
