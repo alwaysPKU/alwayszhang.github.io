17236320eb2876721a7fa61d2af0ca06b89ae7cb
用户3031733109593 <961445003792483-3031733109593@noreply.coze.cn>
2026-07-15 18:09:19 +0800

    fix: 改回 trailingSlash: true 生成目录结构

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,7 @@ const nextConfig: NextConfig = {
   // outputFileTracingRoot: path.resolve(__dirname, '../../'),  // Uncomment and add 'import path from "path"' if needed
   /* config options here */
   output: process.env.GITHUB_PAGES === 'true' ? 'export' : undefined,
-  trailingSlash: false,
+  trailingSlash: true,
   allowedDevOrigins: ['*.dev.coze.site'],
   images: {
     remotePatterns: [
